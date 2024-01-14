a2cddb5597537c437d780cea9a35464dfd23fc67
byhebsoares <96448660+byhebsoares@users.noreply.github.com>
2024-01-14 10:23:02 -0300

    att

--- 1_introducao/1_ola_mundo/exemplo1.kt
+++ 1_introducao/1_ola_mundo/exemplo1.kt
@@ -5,7 +5,9 @@
  */
 
 package me.dio.helloworld           // 1. Definição do pacote, algo muito comum para organização de código (apesar de opcional).
+fun exemplo1(){
 
+}
 fun main() {                        // 2. Ponto de entrada de um código Kotlin, definido pela função [main].
     println("Olá, Mundo!")          // 3. Imprime na saída padrão um texto usando a função [println] (importada implicitamente).
 }
